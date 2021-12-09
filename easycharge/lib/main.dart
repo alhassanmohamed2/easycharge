feb0e0701e6b0687680c27e0ae2aeede680df455
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2021-12-10 01:44:00 +0200

    Main code
    Doaa Gamal Desgin (About Page)

--- easycharge/lib/main.dart
+++ easycharge/lib/main.dart
@@ -1,115 +1,197 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: AboutPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(),
+      drawer: const Drawer(),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.all(4)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/mohamed.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Mohamed Mohamed Atef Amhawy",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/alhassan.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Alhassan Mohamed",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/mahmoud.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Mahmoud Nagy Elsayed",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/sara.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Sara Abdulah Nassar",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/alaa.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Alaa Mohamed Mohamed Mahdy",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/doaa_mustfa.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Duaa Mostafa Abdulbaset",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5)),
+              Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade300),
+                    color: Colors.purple.shade100),
+                child: Row(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/doaa_gamal.jfif"),
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(6),
+                        child: Text("Doaa Gamal Eltohamy",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.black)))
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Card(
+            child: Text(
+              "This program helps you to charge the cards with ease, by placing the 15-digit charging number on the rectangle that appears to you when you click on the camera, and the program will charge the card immediately ",
+              style: TextStyle(color: Colors.white70, fontSize: 18),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            margin: EdgeInsets.all(15),
+            elevation: 15,
+            color: Colors.purple.shade300,
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
