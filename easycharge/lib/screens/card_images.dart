29d438698d73126476f900f228067b5d25f44dfc
Alhassan Mohamed <alhassan200012@gmail.com>
2026-09-11 04:25:53 +0300

    AI Improved: Removed 404 error gif from empty state

--- easycharge/lib/screens/card_images.dart
+++ easycharge/lib/screens/card_images.dart
@@ -149,7 +149,7 @@ class _CardImagesState extends State<CardImages> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Image.asset('assets/error.gif', width: 200, height: 200),
+                        Icon(Icons.credit_card_off_rounded, size: 80, color: Colors.grey.shade300),
                         const SizedBox(height: 16),
                         Text(
                           tr("No Cards Scanned"),
